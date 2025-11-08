03d1da440b09436a05043ab615f93a2db5e3d5a8
Thando Somacele <thando.somacele@gmail.com>
2025-11-08 13:56:13 +0200

    security: Remove .svelte-kit build output from version control
    The .svelte-kit directory contained compiled code with embedded environment variables including the OpenRouter API key. This directory should never be committed as it's auto-generated during build time and contains sensitive information.
    
    - Remove all .svelte-kit files from git tracking
    - .svelte-kit already in .gitignore (was added previously)
    - Build output will be regenerated locally on each build

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DNXFkvqx.js","_app/immutable/chunks/CDixFzPb.js","_app/immutable/chunks/DBl43UXo.js","_app/immutable/chunks/DZAX65-V.js","_app/immutable/chunks/D88Rs6nj.js","_app/immutable/chunks/BK7kGjNc.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.Cmvn9ESU.js","_app/immutable/chunks/CDixFzPb.js","_app/immutable/chunks/DBl43UXo.js","_app/immutable/chunks/DZAX65-V.js","_app/immutable/chunks/Di6iWIYm.js","_app/immutable/chunks/DQHJ3VSg.js","_app/immutable/chunks/CxRqkpOG.js"];
-export const stylesheets = ["_app/immutable/assets/2.Ckpx1aZ6.css"];
-export const fonts = [];
